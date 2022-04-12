bf261556c4e3b71b71df0f71bd266057a2d41756
kevlardan <dmillion7@gmail.com>
2022-04-12 15:56:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,15 @@
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	_putchar('B');
-	_putchar('e');
-	_putchar('t');
-	_putchar('t');
-	_putchar('y');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
 
 	return (0);
 }
